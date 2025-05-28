6ed7e068dbf76a24ad7690630c0056801c6abddc
veer842 <rajputvirendra82213@gmail.com>
2025-05-28 17:29:58 +0530

    change in rock code

--- Second-Project/app.js
+++ Second-Project/app.js
@@ -1,41 +1,46 @@
 let choices = document.querySelectorAll(".choice");
 let msg = document.querySelector("#msg");
-
+let userScore = document.querySelector("#score-user");
+let compScore = document.querySelector("#score-comp");
+let usersc = 0;
+let compsc = 0;
 const compchoose = ()=>{
     let option =["paper","rock","scissor"]
     let indx = Math.floor(Math.random()*3);
     return option[indx];
 }
-
 const showinner = (winner)=>{
 if(winner){
-    msg.innerText = "congratulation you have WON";
-    msg.style.backgroundColor = "green";
-    console.log("you are winner");
+ msg.innerText = "congratulation you have WON"; 
+  msg.style.backgroundColor = "green"; 
+ userScore.innerText = ++usersc;
+   console.log("winner");
 } else{
-    msg.innerText = "Sorry! you have LOSE";
-    msg.style.backgroundColor = "red";
-console.log("you are looser");
+     msg.innerText = "sorry! you have lose";
+      msg.style.backgroundColor = "red";
+ compScore.innerText = ++compsc;
+   console.log("lose");
 }
 }
+
 
 const playGame = (userchoice)=>{
 let compchoice = compchoose();
-// console.log(userchoice);
-// console.log(compchoice);
+console.log(userchoice);
+console.log(compchoice);
 
 if(userchoice === compchoice){
      msg.innerText = "The Game is Draw! Try Again";
       msg.style.backgroundColor = "#258276";
     console.log("game drow");
-}else{
+} else {
     let winner = true;
     if(userchoice === "rock"){
-      winner = compchoice ==="papper" ? false : true;
+      winner = compchoice == "paper" ? false : true;
     }else if(userchoice === "paper"){
-         winner = compchoice ==="scissor" ? false : true;
+         winner = compchoice == "scissor" ? false : true;
     } else {
- winner = compchoice ==="rock" ? false : true;
+ winner = compchoice == "rock" ? false : true;
     }
     showinner(winner);
 }
